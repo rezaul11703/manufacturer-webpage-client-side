5e5566b31387682d8f7dff662b0aa72a46962e17
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-28 23:54:01 +0600

    try last

--- src/Home/Reviews.js
+++ src/Home/Reviews.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
-  fetch("");
+  fetch("http://localhost:5000/userReviews")
+    .then((res) => res.json())
+    .then((data) => setReviews(data));
   return (
     <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <h2 className="card-title"> Reviewer NAme</h2>
-        <p>Rating :</p>
-        <p> Comment : </p>
-        <div className="card-actions"></div>
-      </div>
+      {reviews.map((r) => (
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <h2 className="card-title"> Reviewer NAme :{r.reviewersName}</h2>
+          <p>Rating :{r.reviewersRating}</p>
+          <p> Comment :{r.reviewersFeelings} </p>
+          <div className="card-actions"></div>
+        </div>
+      ))}
     </div>
   );
 };
