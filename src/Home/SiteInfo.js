527b3a13bc46637c64c5870551a669661b00d8ac
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-29 14:29:50 +0600

    Uddated

--- src/Home/SiteInfo.js
+++ src/Home/SiteInfo.js
@@ -3,11 +3,11 @@ import React from "react";
 const SiteInfo = () => {
   return (
     <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col lg:flex-row">
+      <div class="hero-content flex-col lg:flex-row justify-center align-center">
         <img src="https://i.ibb.co/64gYqYk/download.jpg" />
-        <div>
+        <div className="container w-80 ">
           <h1 class="text-2xl font-bold">About Us</h1>
-          <span class="p-4 text-center">
+          <span class="">
             We Provide Best Quality computer parts with demand and delivered
             with in a time . We have some minimum ordered items . So just
             Explore it !!! Hope a good journey with Us{" "}
